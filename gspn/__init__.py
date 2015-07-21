1bbac9f0bb295cb521d4389d1099d85a3d4c05f6
Andrew Dolgert <adolgert@cornell.edu>
2015-07-21 11:02:50 -0400

    Able to use from outside the package.

--- gspn/__init__.py
+++ gspn/__init__.py
@@ -1,2 +1,5 @@
 from .llcp import LLCP
 from .sample import NextReaction, NextReactionRecord, FirstReaction
+from .runner import RunnerFSM
+from .distributions import ExponentialDistribution, WeibullDistribution
+from .point_process import poisson_point_process_2D, thomas_point_process_2D
